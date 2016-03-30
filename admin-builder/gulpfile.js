e4dd6d798383439666f3da51852d9284c9de0df3
Ciprian Turcu <turcuciprian1@gmail.com>
2016-03-30 11:37:51 +0300

    update .1

--- admin-builder/gulpfile.js
+++ admin-builder/gulpfile.js
@@ -2,27 +2,53 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglifyjs');
+//***********
+//
+// Watchable files
+//
+//***********
 
-
+//
+//SASS
+//
 gulp.task('sass', function () {
-  return gulp.src('./src/sass/**/*.scss')
+  return gulp.src('./src/sass/custom/*.scss')
   .pipe(sourcemaps.init())
   .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
   .pipe(sourcemaps.write())
   .pipe(gulp.dest('./ss'));
 });
+//
+// Javascript
+//
 
 gulp.task('compressjS', function() {
-  return gulp.src('./src/js/**/*.js')
-  .pipe(uglify('all.min.js', {
-    outSourceMap: true
+  return gulp.src('./src/js/custom/*.js')
+  .pipe(uglify('script.min.js', {
+    outSourceMap: true,
+    mangle: false
   }))
   .pipe(gulp.dest('./ss'));
 });
 
+//
+// First run files
+//
+
+//js libraries
+gulp.task('compressJsLibs', function() {
+  return gulp.src('./src/js/lib/*.js')
+  .pipe(uglify('lib.min.js', {
+    outSourceMap: true,
+    mangle: false
+  }))
+  .pipe(gulp.dest('./ss'));
+});
+
+
 gulp.task('sass:watch', function () {
   gulp.watch('./src/sass/**/*.scss', ['sass']);
-  gulp.watch('./src/js/**/*.js', ['compressjS']);
+  gulp.watch('./src/js/custom/*.js', ['compressjS']);
 
 });
-gulp.task('default', ['sass','compressjS','sass:watch']);
+gulp.task('default', ['sass','compressjS','compressJsLibs','sass:watch']);
